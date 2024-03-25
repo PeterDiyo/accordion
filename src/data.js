030ac59cb7a2af354439b7d7a3134a0b80f28505
unknown <PeterDiyo>
2024-03-25 17:34:11 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    title: "What is a Framework?",
+    info: "A framework is a tool that gives software developers access to prebuilt components or solutions designed to expedite development.",
+  },
+  {
+    id: 2,
+    title: "Name the stages of the software development lifecycle (SDLC)",
+    info: "Planning, Requirements gathering and analysis, Design, Coding and implementation, Testing, Deployment, Maintenance, Like the question above, this one tests your knowledge. As long as you outline the stages, that's usually sufficient.",
+  },
+  {
+    id: 3,
+    title: "What is Refactoring?",
+    info: "Refactoring is the process of taking care of existing code and restructuring it, typically to improve the code through small changes without altering its underlying behavior.",
+  },
+  {
+    id: 4,
+    title: "Have you ever created unit tests?",
+    info: "Unit testing involves taking small parts of an application — referred to as units — and independently scrutinizing them to ensure correct operation.",
+  },
+  {
+    id: 5,
+    title: "What is CORS?",
+    info: "CORS, or cross-origin resource sharing, is an HTTP-header-based mechanism that lets a server define origins outside of itself where the browser should allow the loading of resources. Within CORS is a mechanism that ensures browsers reach out before a user request to confirm the server will support the request for the cross-origin resource.",
+  },
+];
+export default questions;
